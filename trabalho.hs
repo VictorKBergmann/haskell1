da73e45ef8f1a702ad5f222037869716576a0a52
VictorKBergmann <48692371+VictorKBergmann@users.noreply.github.com>
2019-04-22 21:02:58 -0300

    da dando erro na conta

--- trabalho.hs
+++ trabalho.hs
@@ -3,3 +3,13 @@ takewhile x [] = []
 takewhile x (a:z)
         |a == x = []
         |otherwise = a : (takewhile x z)
+
+
+
+tira :: [String] -> String
+tira [] = ""
+tira (t:x) = t
+
+conta :: [[String]] -> [(String,Int)]
+conta [] = []
+conta (a:x) = ( tira a , length(a)) : (conta (x))
