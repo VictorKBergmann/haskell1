cc4af7f456682791217975e39fbd7ac8c461663c
VictorKBergmann <48692371+VictorKBergmann@users.noreply.github.com>
2019-04-22 22:06:20 -0300

    só o takewhile útil

--- trabalho.hs
+++ trabalho.hs
@@ -1,9 +1,16 @@
+criaEspaso :: (String,Int) -> Int -> String
+criaEspaso ( ( a : z ) ,b ) x 
+        | [a] == ""            = ""
+        | b == x                = ""
+        | x - b > 0 && a /= ' ' = [a] ++ criaEspaso (z,b+1) (x-1)
+        | otherwise             = [a] ++ " " ++ criaEspaso (z,b+1) (x-1)
+         --x - b > 0 && a == ' '
+
 takewhile :: Char-> String->String
 takewhile x [] = []
 takewhile x (a:z)
         |a == x = []
         |otherwise = a : (takewhile x z)
-
 
 
 tira :: [String] -> String
@@ -13,5 +20,3 @@ tira (t:x) = t
 conta :: [[String]] -> [(String,Int)]
 conta [] = []
 conta (a:x) = ( tira a , length(a)) : (conta (x))
-
-
