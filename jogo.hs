680f9b606bc8ffb13438d1b48bb787c0b5f7bf2d
VictorKBergmann <48692371+VictorKBergmann@users.noreply.github.com>
2019-07-02 20:33:40 -0300

    02/07

--- jogo.hs
+++ jogo.hs
@@ -1,144 +1,89 @@
+
 import System.Console.ANSI
 import Data.Char
 import System.IO
 import Control.Concurrent
 
-main                    :: IO ()
-main =  do hSetEcho stdin False
-           hideCursor
-           clearScreen
-           hPutChar stdout '*'
-           move
-
-move :: IO ()
-move = do  c <- hGetChar stdin
-           case (ord c) of
-             114 -> moveUp
-             99  -> moveDown
-             100 -> moveLeft
-             102 -> moveRight
-             _  -> return ()
-
-
-moveUp :: IO ()
-moveUp = do 
-       clearScreen
-       hCursorBackward stdout 1
-       hCursorUp stdout 1
-       hPutChar stdout '*'
-       test
-
-moveDown :: IO ()
-moveDown = do 
-       clearScreen
-       hCursorBackward stdout 1
-       hCursorDown stdout 1
-       hPutChar stdout '*'
-       move
-moveLeft :: IO ()
-moveLeft = do 
-       clearScreen
-       hCursorBackward stdout 1
-       hCursorBackward stdout 1
-       hPutChar stdout '*'
-       move
-
-moveRight :: IO ()
-moveRight = do 
-       clearScreen
-       hCursorForward stdout 1
-       hPutChar stdout '*'
-       test
-
-
-test                    :: IO ()
-test =  do hSetEcho stdin False
-           hideCursor
-           clearScreen
-           hPutChar stdout '*'
-           test1
-
-test1 :: IO ()
-test1 = do threadDelay 100000
-           moveUp
-           test1
-
-
-
-
-
-
-
-
-
------------------------------------
 
 data Diresao = Esq_cima| Esq_baixo | Dir_cima| Dir_baixo
-        deriving(Eq,Show)
+    deriving(Eq,Show)
 
+main :: IO()
+main = do
+  setCursorPosition 30 10
+  leftUp
+  clearScreen
+  threadDelay 1000000
 
 leftUp :: IO ()
 leftUp = do 
-   ClearFromCursorToBegining
-   cursorUpLine 1 
-   cursorBackward 2
-   putChar '@'
-   savecrusor
+   threadDelay 1000000
+   clearFromCursorToScreenBeginning
+   hCursorUp stdout 1
+   hCursorBackward stdout 2
+   hPutChar stdout '@'
+   saveCursor
    --skateMove
    restoreCursor
-   direct Esq_cima
+   directEC
 
 
 rigthUp :: IO ()
 rigthUp = do 
-   ClearFromCursorToBegining
+   threadDelay 1000000
+   clearFromCursorToScreenBeginning
    cursorUpLine 1 
-   putChar '@'
-   savecrusor
+   hPutChar stdout '@'
+   saveCursor
    --skateMove
    restoreCursor
-   direct Dir_cima
+   directDC
 
 rigthDown :: IO ()
 rigthDown = do 
-   ClearFromCursorToBegining
+   threadDelay 1000000
+   clearFromCursorToScreenBeginning
    cursorDownLine 1 
-   putChar '@'
-   savecrusor
+   hPutChar stdout '@'
+   saveCursor
    --skateMove
    restoreCursor
-   direct Dir_baixo
+   directDB
    
 leftDown :: IO ()
 leftDown = do 
-   ClearFromCursorToBegining
+   threadDelay 1000000
+   clearFromCursorToScreenBeginning
    cursorDownLine 1
    cursorBackward 2 
-   putChar '@'
-   savecrusor
+   hPutChar stdout '@'
+   saveCursor
    --skateMove
    restoreCursor
-   direct Esq_baixo
+   directEB
    
-direct :: Diresao ->IO()
-direct Esq_cima = do
-   bolinha <- getCursorPosition0
-   terminal <-getTerminalSize
-   case bolinha of 
-   	 Just(x,y)->
-   	 case terminal of
-       Just(a,b)->
-       if x>=a 
-       then 
-         if y>=b
-         then
-           leftDown
-         else do
-           leftUp
-       else do 
-       	 if y>=b
-       	 then
-           rigthDown
-         else do
-           rigthUp
+directEC :: IO()
+directEC = do
+  bolinha <- getCursorPosition0
+  terminal <-getTerminalSize
+  case bolinha of 
+    Just(x,y)-> case terminal of
+                    Just(a,b)-> if x>=0 
+                                then 
+                                  if y>=b
+                                  then
+                                     leftDown
+                                  else leftUp
+                                else if y>=b
+                                     then rigthDown
+                                     else rigthUp
 
+directEB :: IO()
+directEB = return() 
+
+
+directDB :: IO()
+directDB = return()
+
+directDC :: IO()
+directDC = return()
